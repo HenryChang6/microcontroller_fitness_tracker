f64504bf425b52aae795c3967df8a2ff536dc2ce
HenryCapyy <henry930619@gmail.com>
2025-01-15 23:41:05 +0800

    delete unnecessary data

--- embedded_sys_integrate/test.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h> 
-#include "dataConfig.h"
-
-void main(void) {
-    printf("step num: %d\n", get_stepNum());
-    printf("temperature: %2f\n", get_temperature());
-    printf("beats: %2f\n", get_beats());
-}
